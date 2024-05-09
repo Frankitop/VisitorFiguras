c1001309836e6120e8d9a7898740eb5ae05c62e3
Franco Piriz Menoni <fpiriz88@gmail.com>
2024-05-09 19:36:05 -0300

    Refactor

--- VisitorFiguras/obj/Debug/net8.0/VisitorFiguras.AssemblyInfo.cs
+++ VisitorFiguras/obj/Debug/net8.0/VisitorFiguras.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisitorFiguras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7329b86cbd0294d8975bf446d0d228b9f8cdb03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d39787b0dd05455a5d601bc5a718976bb3e63d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisitorFiguras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisitorFiguras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
